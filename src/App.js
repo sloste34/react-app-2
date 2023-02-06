108d4c055baa7f504687d13dd3ef7369fc84ba71
sloste34 <cocacolazero024@gmail.com>
2023-02-06 23:32:05 +0900

    setting router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Route } from 'react-router-dom';
+
+import AllMeetupPage from './pages/AllMeetup';
+import FavoritePage from './pages/Favorites';
+import NewMeetupPage from './pages/NewMeetup';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div>
+      <Route path='/'>
+        <AllMeetupPage />
+      </Route>
+      <Route path='/newMeetUp'>
+        <NewMeetupPage />
+      </Route>
+      <Route path='/favorites'>
+        <FavoritePage />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
