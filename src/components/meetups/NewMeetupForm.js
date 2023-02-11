952918e1d2dce20d59660a70eb9e4dca4f79b6fd
sloste34 <cocacolazero024@gmail.com>
2023-02-11 16:10:46 +0900

    class 40.Working With Forms

---
+++ src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,32 @@
+import styles from './NewMeetupForm.module.css';
+import Card from '../ui/Card';
+
+function NewMeetupForm() {
+  return (
+    <Card>
+      <form className={styles.form}>
+        <div className={styles.control}>
+          <label htmlFor='title'>Meetup Title</label>
+          <input type='text' required id='title'></input>
+        </div>
+        <div className={styles.control}>
+          <label htmlFor='image'>Meetup Image</label>
+          <input type='url' required id='image' />
+        </div>
+        <div className={styles.control}>
+          <label htmlFor='address'>Address</label>
+          <input type='text' required id='address' />
+        </div>
+        <div className={styles.control}>
+          <label htmlFor='description'>Description</label>
+          <textarea id='description' required rows='5'></textarea>
+        </div>
+        <div className={styles.action}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </Card>
+  );
+}
+
+export default NewMeetupForm;
